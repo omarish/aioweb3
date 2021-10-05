3d9d864aaff044e62fae03855c26c542f49d8118
Omar Bohsali <omar.bohsali@gmail.com>
2021-10-05 12:02:09 -0700

    Adds worker pool

--- tests/test_worker_pool.py
+++ tests/test_worker_pool.py
@@ -1,29 +1,32 @@
 import asyncio
 from asyncio import Queue
 
-from aioweb3 import AsyncWeb3, async_web3
+from aioweb3 import AsyncWeb3
 
 
-async def producer(queue: Queue, jobs: int = 1000):
-    print("producing")
+async def producer(queue: Queue, jobs: int):
+    print(f"Enqueue {jobs} jobs")
     for index in range(jobs):
         await queue.put(index)
 
 
 async def consumer(queue: Queue, index: int):
-    print(f"Consumer {index} booting")
+    def log(*args, **kwargs):
+        print(f"Worker {index}: ", *args, **kwargs)
+    log("Boot")
+    web3 = AsyncWeb3()
     while 1:
         await queue.get()
-        print("getting version")
-        version = await async_web3.eth.get_block("latest")
-        print("got version")
+        block = await web3.eth.get_block("latest")
+        log("latest block: ", block.number)
         queue.task_done()
 
 
-async def entrypoint(jobs: int = 1_000, workers: int = 1):
+async def entrypoint(jobs: int = 5_000, workers: int = 50):
     queue = Queue()
     producers = [asyncio.create_task(producer(queue, jobs))]
     consumers = [asyncio.create_task(consumer(queue, i)) for i in range(workers)]
+
     await asyncio.gather(*producers)
     await queue.join()
 
